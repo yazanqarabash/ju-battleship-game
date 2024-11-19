d8b17602b4003a7d3bf1d63c3c35db81ca5585b1
Yazan Qarabash <yazan@cybersaurus.net>
2024-11-19 15:30:54 +0100

    feat: create project structure

--- app/src/main/java/com/ju/battleshipgame/MainActivity.kt
+++ app/src/main/java/com/ju/battleshipgame/MainActivity.kt
@@ -7,10 +7,8 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.ju.battleshipgame.ui.home.Greeting
 import com.ju.battleshipgame.ui.theme.BattleshipGameTheme
 
 class MainActivity : ComponentActivity() {
@@ -30,18 +28,3 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "$name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    BattleshipGameTheme {
-        Greeting("Android")
-    }
-}
---
+++ app/src/main/java/com/ju/battleshipgame/ui/home/HomeScreen.kt
@@ -0,0 +1,23 @@
+package com.ju.battleshipgame.ui.home
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.ju.battleshipgame.ui.theme.BattleshipGameTheme
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "$name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    BattleshipGameTheme {
+        Greeting("Android")
+    }
+}
