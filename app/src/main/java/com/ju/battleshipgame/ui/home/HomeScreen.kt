9b1eba5263832ce88d6a7aef1374fb0ef90cbab5
Mhd Zuhir Almidani <mydanizouher@gmail.com>
2024-11-20 22:00:29 +0100

    feat: create home and lobby
    We have created a home screen where the player can write their name and join the lobby, as well as a lobby screen where all players are displayed. There is also a challenge button to challenge them and a button to change there name

--- app/src/main/java/com/ju/battleshipgame/ui/home/HomeScreen.kt
+++ app/src/main/java/com/ju/battleshipgame/ui/home/HomeScreen.kt
@@ -1,23 +1,73 @@
 package com.ju.battleshipgame.ui.home
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.core.provider.FontsContractCompat.Columns
+import androidx.navigation.NavHostController
+import com.ju.battleshipgame.R
+import com.ju.battleshipgame.models.Player
 import com.ju.battleshipgame.ui.theme.BattleshipGameTheme
-
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "$name!",
-        modifier = modifier
-    )
-}
+fun Homescreen(
+    list: MutableList<Player>,
+    navcontroller: NavHostController,
+    modifier: Modifier = Modifier
+) {
+    var playerName by remember { mutableStateOf(" ") }
+    Box(
+        modifier = modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Image(
+            painter = painterResource(id= R.drawable.battleship_home),
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier.fillMaxSize()
+        )
+        Column(
+            modifier = modifier
+                .fillMaxSize()
+                .padding(2.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            TextField(
+                value = playerName,
+                onValueChange = { playerName = it },
+                label = { Text("Enter your name") },
+                modifier = Modifier.fillMaxWidth().padding(bottom = 16.dp)
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    BattleshipGameTheme {
-        Greeting("Android")
+            )
+            Button(
+                onClick = {
+                    val newPlayer = Player(id = list.size.toString(), name = playerName)
+                    list.add(newPlayer)
+                    navcontroller.navigate("LobbyScreen")
+                },
+                enabled = playerName.trim().isNotEmpty(),
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Text("Join the Lobby")
+            }
+        }
     }
 }
